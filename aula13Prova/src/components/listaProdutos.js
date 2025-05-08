55d954d5932f146444d76963c447ff4bbdc06f74
João Vitor <jvde.olivera@gmail.com>
2025-05-08 01:16:31 -0300

    BackEnd tela lista produtos - Pronta

---
+++ aula13Prova/src/components/listaProdutos.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+
+const ListaProdutos = () => {
+
+    var [produtos, setProdutos] = useState([])
+
+    const ListarProdutos = async () => {
+        var url = "https://backend-completo.vercel.app/app/produtos"
+
+        var token = localStorage.getItem("ALUNO_ITE")
+
+        await axios.get(
+            url,
+            { headers: { Authorization: `Bearer ${token}` } }
+        ).then(retorno => {
+            console.log(retorno)
+            if (retorno.data.error) {
+                alert(retorno.data.error)
+                return
+            }
+            if (retorno.status === 200) {
+                alert("Listagem de Produtos - sucesso.")
+                setProdutos(retorno.data)
+                console.log(retorno)
+            }
+        })
+    }
+
+    return (
+        <div>
+            <h1>Produtos</h1>
+
+            <input type="button" value="Listar Produtos" onClick={() => ListarProdutos()} />
+
+
+            <table border='1'>
+                <thead>
+                    <tr>
+                        <th>Nome do Produto</th>
+                        <th>usuario</th>
+                        <th>id</th>
+                        <th>Descrição</th>
+                        <th>imagem</th>
+                        <th>preço</th>
+                        <th>quantidade</th>
+                        <th>Editar</th>
+                        <th>Excluir</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {produtos.map((produto, indiceDoProduto) => (
+                        <tr key={indiceDoProduto}>
+                            <td>{produto.nome} </td>
+                            <td>{produto.usuario}</td>
+                            <td>{produto._id}</td>
+                            <td>{produto.descricao}</td>
+                            <td>{produto.imagem}</td>
+                            <td>{produto.preco}</td>
+                            <td>{produto.quantidade}</td>
+                            <td><input type="button" value="Editar" /></td>
+                            <td><input type="button" value="Excluir" /></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ListaProdutos
