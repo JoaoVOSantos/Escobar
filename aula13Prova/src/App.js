702a15be50011a52c0f2ef79ff93a399e77c5d63
João Vitor <jvde.olivera@gmail.com>
2025-05-08 01:17:10 -0300

    Fazendo Rotas das Telas

--- aula13Prova/src/App.js
+++ aula13Prova/src/App.js
@@ -5,6 +5,10 @@ import Produtos from './components/produtos';
 import Pessoas from './components/pessoas';
 import Login from './components/login';
 import Registrar from './components/registrar';
+import Categorias from './components/categorias';
+import ListaCategorias from './components/listaCategorias';
+import ListaProdutos from './components/listaProdutos';
+import EditaProdutos from './components/editaProdutos';
 
 import {
     BrowserRouter,
@@ -14,6 +18,7 @@ import {
     Outlet,
     Link
 } from 'react-router-dom';
+
 
 
 const Middleware = () => {
@@ -36,14 +41,22 @@ const App = () => {
                     <Link to="/pessoas">Pessoas</Link>
                     <Link to="/registrar">Registrar</Link>
                     <Link to="/login">Login</Link>
+                    <Link to="/categorias">Categorias</Link>
+                    <Link to="/listaCategorias">Lista de Categorias</Link>
+                    <Link to="/listaProdutos">Lista de Produtos</Link>
+                    <Link to="/editaProdutos">Editar Produtos</Link>
                 </nav>
 
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/registrar" element={<Registrar />} />
+                    <Route path="/listaCategorias" element={<ListaCategorias />} />
+                    <Route path="/listaProdutos" element={<ListaProdutos />} /> 
+                    <Route path="/editaProdutos" element={<EditaProdutos />} /> 
                     <Route element={<Middleware />}>
                         <Route path="/" element={<Dashboard />} />
                         <Route path="/produtos" element={<Produtos />} />
+                        <Route path="/categorias" element={<Categorias />} />
                         <Route path="/pessoas" element={<Pessoas />} />
                     </Route>
                 </Routes>
