4a4bf87265bb7d97f00e79ef29f7619049e7a35e
João Vitor <jvde.olivera@gmail.com>
2025-05-09 02:07:12 -0300

    Adicionando Mais Rotas

--- aula13Prova/src/App.js
+++ aula13Prova/src/App.js
@@ -6,9 +6,12 @@ import Pessoas from './components/pessoas';
 import Login from './components/login';
 import Registrar from './components/registrar';
 import Categorias from './components/categorias';
+import Vendas from './components/vendas';
 import ListaCategorias from './components/listaCategorias';
 import ListaProdutos from './components/listaProdutos';
 import EditaProdutos from './components/editaProdutos';
+import Limpar from './components/limpar';
+
 
 import {
     BrowserRouter,
@@ -37,27 +40,34 @@ const App = () => {
             <div>
                 <nav style={{ display: "flex", gap: "10px" }}>
                     <Link to="/">Dashboard</Link>
-                    <Link to="/produtos">Produtos</Link>
                     <Link to="/pessoas">Pessoas</Link>
                     <Link to="/registrar">Registrar</Link>
                     <Link to="/login">Login</Link>
+                    <Link to="/produtos">Produtos</Link>
                     <Link to="/categorias">Categorias</Link>
+                    <Link to="/vendas">Vendas</Link>
                     <Link to="/listaCategorias">Lista de Categorias</Link>
                     <Link to="/listaProdutos">Lista de Produtos</Link>
                     <Link to="/editaProdutos">Editar Produtos</Link>
+                    <Link to="/limpar">limpar</Link>
                 </nav>
 
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/registrar" element={<Registrar />} />
-                    <Route path="/listaCategorias" element={<ListaCategorias />} />
-                    <Route path="/listaProdutos" element={<ListaProdutos />} /> 
-                    <Route path="/editaProdutos/:codigo" element={<EditaProdutos />} /> 
                     <Route element={<Middleware />}>
                         <Route path="/" element={<Dashboard />} />
                         <Route path="/produtos" element={<Produtos />} />
                         <Route path="/categorias" element={<Categorias />} />
                         <Route path="/pessoas" element={<Pessoas />} />
+                        <Route path="/listaCategorias" element={<ListaCategorias />} />
+                        <Route path="/listaProdutos" element={<ListaProdutos />} />
+                        <Route path="/editaProdutos/:codigo" element={<EditaProdutos />} />
+                        {/* <Route path="/excluiProdutos/:codigo" element={<EditaProdutos />} /> */}
+                        <Route path="/editaCategorias/:codigo" element={<EditaProdutos />} />
+                        {/* <Route path="/excluiCategorias/:codigo" element={<EditaProdutos />} /> */}
+                        <Route path="/vendas" element={<Vendas />} />
+                        <Route path="/limpar" element={<Limpar />} />
                     </Route>
                 </Routes>
             </div>
