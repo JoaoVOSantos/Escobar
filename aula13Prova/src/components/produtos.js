0e49a9c95451ed21164d9bfefac0f4d6b29aa422
Joao Vitor <jvde.olivera@gmail.com>
2025-05-11 17:36:07 -0300

    Front end Tela Produtos - Pronta

--- aula13Prova/src/components/produtos.js
+++ aula13Prova/src/components/produtos.js
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import Radio from '@mui/material/Radio';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
 
 const Produtos = () => {
     var [nome, setNome] = useState('')
@@ -10,6 +21,10 @@ const Produtos = () => {
     var [imagem, setImagem] = useState('')
 
     var [categorias, setCategorias] = useState([])
+
+    const [mensagem, setMensagem] = useState('');
+    const [open, setOpen] = useState(false);
+    const [erro, setErro] = useState(false);
 
     useEffect(() => {
         listarCategoria();
@@ -25,12 +40,14 @@ const Produtos = () => {
             { headers: { Authorization: `Bearer ${token}` } }
         ).then(retorno => {
             if (retorno.data.error) {
-                alert(retorno.data.error)
+                setErro(true)
+                setMensagem(retorno.data.error)
+                setOpen(true)
                 return
             }
             if (retorno.status === 200) {
                 setCategorias(retorno.data)
-                console.log(retorno)
+                console.log(retorno.data)
             }
         })
     }
@@ -55,34 +72,142 @@ const Produtos = () => {
             { headers: { Authorization: `Bearer ${token}` } }
         ).then(retorno => {
             if (retorno.data.error) {
-                alert(retorno.data.error)
+                setErro(true)
+                setMensagem(retorno.data.error)
+                setOpen(true)
                 return
             }
             if (retorno.data._id) {
-                alert("Prduto cadastrado com sucesso.")
-                console.log(retorno)
+                setErro(false)
+                setMensagem("Produto cadastrado com sucesso.")
+                setOpen(true)
             }
         })
     }
 
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <div>
-            <h1>Produtos</h1>
-            <input type="text" placeholder="Nome do produto" onChange={(e) => setNome(e.target.value)} />
-            <input type="number" placeholder="quantidade" onChange={(e) => setQuantidade(e.target.value)} />
-            <input type="number" placeholder="preço" onChange={(e) => setPreco(e.target.value)} />
+        // "div" vazia pq tem que retornar apenas uma div
+        // Conhecido como  Fragmento React ou Fragment
+        <>
+            {/* "Div" da tela inteira  */}
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: 'calc(100vh - 72px)',
+                    bgcolor: '#f0f0f0',
+                }}
+            >
+                {/* "Div" envolvendo todo o conteudo */}
+                <Box
+                    sx={{
+                        width: 400,
+                        p: 4,
+                        borderRadius: 2,
+                        border: '2px solid #1976d2',
+                        bgcolor: 'white',
+                        textAlign: 'center',
+                    }}
+                >
+                    {/* Titulo */}
+                    <Typography variant="h5" mb={2}>
+                        Cadastrar Produto
+                    </Typography>
 
-            {categorias.map((categoria, indice) => (
-                <div key={indice}>
-                    <input type="radio" name="categoria" onChange={(e) => setCategoria(e.target.value)} /> 
-                    {categoria.nome}
-                </div>
-            ))}
+                    {/* Div do Nome e Quantidade */}
+                    <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
+                        <TextField
+                            fullWidth
+                            label="Nome do produto"
+                            variant="outlined"
+                            onChange={(e) => setNome(e.target.value)}
+                        />
+                        <TextField
+                            fullWidth
+                            label="Quantidade"
+                            type="number"
+                            variant="outlined"
+                            onChange={(e) => setQuantidade(e.target.value)}
+                        />
+                    </Box>
 
-            <input type="text" placeholder="descricao" onChange={(e) => setDescricao(e.target.value)} />
-            <input type="text" placeholder="imagem" onChange={(e) => setImagem(e.target.value)} />
-            <input type="button" value="Cadastrar Produto" onClick={() => cadastrarProduto()} />
-        </div>
+                    {/* div do Preço e Descrição */}
+                    <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
+                        <TextField
+                            fullWidth
+                            label="Preço"
+                            type="number"
+                            variant="outlined"
+                            onChange={(e) => setPreco(e.target.value)}
+                        />
+                        <TextField
+                            fullWidth
+                            label="Descrição"
+                            variant="outlined"
+                            onChange={(e) => setDescricao(e.target.value)}
+                        />
+                    </Box>
+
+                    {/* Campo Imagem/URl */}
+                    <TextField
+                        fullWidth
+                        label="Imagem (URL)"
+                        variant="outlined"
+                        sx={{ mb: 2 }}
+                        onChange={(e) => setImagem(e.target.value)}
+                    />
+
+                    {/* RadioGroup de Categorias */}
+                    <FormControl
+                        component="fieldset"
+                        sx={{
+                            mb: 2,
+                            width: '100%',
+                            alignItems: 'flex-start',
+                        }}
+                    >
+                        {/* Titulo */}
+                        <FormLabel component="legend">Categorias</FormLabel>
+                        {/*  */}
+                        <RadioGroup
+                            onChange={(e) => setCategoria(e.target.value)}
+                            sx={{ pl: 1 }}
+                        >
+                            {categorias.map((categoria, index) => (
+                                <FormControlLabel
+                                    key={index}
+                                    value={categoria._id}
+                                    control={<Radio />}
+                                    label={categoria.nome}
+                                />
+                            ))}
+                        </RadioGroup>
+                    </FormControl>
+
+                    {/* Botao Cadastrar com OnClick */}
+                    <Button fullWidth variant="contained" color="primary" onClick={cadastrarProduto}>
+                        Cadastrar Produto
+                    </Button>
+                </Box>
+            </Box>
+
+            {/* Mensagem de Erro ou Sucesso */}
+            <Snackbar
+                open={open}
+                autoHideDuration={3000}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: 'center', horizontal: 'center' }}
+            >
+                <Alert onClose={handleClose} severity={erro ? 'error' : 'success'} sx={{ width: '100%' }}>
+                    {mensagem}
+                </Alert>
+            </Snackbar>
+        </>
     )
 }
 
